ad0fd19330544dbee7c22dfc808c9995280b014c
Alfred Allegretti <alfred.allegretti@gmail.com>
2019-10-15 16:00:50 -0400

    ff18 stats and players are hosted locally, can get both data sets on the console

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import "./styles/index.scss";
+import stats18 from "../ffstats18.js";
+import allPlayers from "../ffplayers.js";
+
 
 window.addEventListener("DOMContentLoaded", () => {
   document.getElementById("app").innerText = "Hello World!";
+  console.log(stats18)
+  console.log(allPlayers)
+  // var xmlhttp = new XMLHttpRequest();
+  // xmlhttp.onreadystatechange = function() {
+  //   if (this.readyState == 4 && this.status == 200) {
+  //     var myObj = JSON.parse(this.responseText);
+  //     document.getElementById("demo").innerHTML = myObj;
+  //   }
+  // };
+  // xmlhttp.open("GET", "ffstats.txt", true);
+  // xmlhttp.send();
 });
