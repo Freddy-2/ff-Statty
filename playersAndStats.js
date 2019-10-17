3b5e522f45d92a9b932208a343e959cdf2def6d4
Alfred Allegretti <alfred.allegretti@gmail.com>
2019-10-17 18:05:35 -0400

    have my data structure become what I needed it to be

---
+++ playersAndStats.js
@@ -0,0 +1,51 @@
+// import stats18 from "./ffstats18.js";
+// import allPlayers from "./ffplayers";
+
+
+  
+const calcPlayerStats = function(allPlayers, stats18) {
+  const playersStats = {};
+  // debugger
+  for (let i = 0; i < Object.values(allPlayers).length; i++){
+    if (!!allPlayers[i] && !!stats18[i] && (allPlayers[i].position === "QB" || allPlayers[i].position === "RB" || allPlayers[i].position === "WR" || allPlayers[i].position === "TE" )){
+
+      debugger
+      playersStats[allPlayers[i].search_full_name] = {
+        "fantasy_positions": allPlayers[i].fantasy_positions || null,
+        "number": allPlayers[i].number || null,
+        "full_name": allPlayers[i].search_full_name || null,
+        "first_name": allPlayers[i].first_name || null,
+        "last_name": allPlayers[i].last_name || null,
+        "position": allPlayers[i].position || null,
+        "age": allPlayers[i].age || null,
+        "team": allPlayers[i].team || null,
+        "player_id": allPlayers[i].player_id || null,
+        "my_id": i || null,
+        "gp": stats18[i].gp || 0,
+        "gs": stats18[i].gs || 0,
+        "pts_ppr": stats18[i].pts_ppr || 0,
+        "rec": stats18[i].rec || 0,
+        "rec_yd": stats18[i].rec_yd || 0,
+        "rec_td": stats18[i].rec_td || 0,
+        "rec_tgt": stats18[i].rec_tgt || 0,
+        "rush_att": stats18[i].rush_att || 0,
+        "rush_td": stats18[i].rush_td || 0,
+        "rush_yd": stats18[i].rush_yd || 0,
+        "td": stats18[i].td || 0,
+      } 
+    }
+  }
+  return playersStats;
+} 
+
+// const stats = playersStats(allPlayers, stats18);
+
+// ("use strict");
+// const fs = require("fs");
+// async function doTask() {
+//   let data = JSON.stringify(calcPlayerStats(allPlayers, stats18), null, 2);
+//   fs.writeFileSync("placeholder.json", data);
+// }
+// doTask();
+
+module.exports = calcPlayerStats;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import "./styles/index.scss";
-import stats18 from "../ffstats18.js";
-import allPlayers from "../ffplayers.js";
+// import "./styles/index.scss";
+const stats18 = require("../ffstats18.js");
+const allPlayers = require("../ffplayers.js");
+const playersStats = require("../playersAndStats.js");
+// import fs from "fs";
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   // document.getElementById("app").innerText = "Hello World!";
+//   console.log(stats18[4988]);
+//   console.log(allPlayers[4988]);
+//   console.log(playersStats(allPlayers, stats18));
 
 
-window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("app").innerText = "Hello World!";
-  console.log(stats18.rush_td)
-  console.log(allPlayers.search_full_name)
-  // var xmlhttp = new XMLHttpRequest();
-  // xmlhttp.onreadystatechange = function() {
-  //   if (this.readyState == 4 && this.status == 200) {
-  //     var myObj = JSON.parse(this.responseText);
-  //     document.getElementById("demo").innerHTML = myObj;
-  //   }
-  // };
-  // xmlhttp.open("GET", "ffstats.txt", true);
-  // xmlhttp.send();
-});
+
+//   // var xmlhttp = new XMLHttpRequest();
+//   // xmlhttp.onreadystatechange = function() {
+//   //   if (this.readyState == 4 && this.status == 200) {
+//   //     var myObj = JSON.parse(this.responseText);
+//   //     document.getElementById("demo").innerHTML = myObj;
+//   //   }
+//   // };
+//   // xmlhttp.open("GET", "ffstats.txt", true);
+//   // xmlhttp.send();
+// });
+
+const stats = playersStats(allPlayers, stats18);
+
+"use strict";
+const fs = require("fs");
+// function doTask() {
+  let data = JSON.stringify(stats, null, 2);
+  fs.writeFileSync("placeholder.json", data);
+// }
+// doTask();
